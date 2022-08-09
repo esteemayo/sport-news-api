14f727e899e037e3411b54d6fff3c9893537b9d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-09 18:26:35 +0100

    first commit

--- routes/sports.js
+++ routes/sports.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
+const sportController = require('../controllers/sportController');
+
 const router = express.Router();
+
+router.get('/details/:slug', sportController.getSportBySlug);
+
+router
+  .route('/')
+  .get(sportController.getSports)
+  .post(sportController.createSport);
+
+router
+  .route('/:id')
+  .get(sportController.getSportById)
+  .patch(sportController.updateSport)
+  .delete(sportController.deleteSport);
 
 module.exports = router;
