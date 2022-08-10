345c46f316f0da48d5daacfdf1cdbf458480fe39
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-11 00:36:55 +0100

    fixed a bug

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -10,6 +11,10 @@ router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/reset-password/:token', authController.resetPassword);
 
-router.post('/update-my-password', authController.updatePassword);
+router.patch(
+  '/update-my-password',
+  authMiddleware.protect,
+  authController.updatePassword
+);
 
 module.exports = router;
