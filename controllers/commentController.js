b34c4de8e4593817e46ded9f5e91e378a48e2247
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-11 02:46:11 +0100

    first commit

---
+++ controllers/commentController.js
@@ -0,0 +1,71 @@
+const { StatusCodes } = require('http-status-codes');
+const asyncHandler = require('express-async-handler');
+
+const Comment = require('../models/Comment');
+const NotFoundError = require('../errors/notFound');
+const factory = require('./handlerFactory');
+const ForbiddenError = require('../errors/forbidden');
+
+exports.updateComment = asyncHandler(async (req, res, next) => {
+  const { id: commentId } = req.params;
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    return next(
+      new NotFoundError(`No comment found with that ID →→ ${commentId}`)
+    );
+  }
+
+  if (comment.user._id === String(req.user.id) || req.user.role === 'admin') {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      { $set: { ...req.body } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      comment: updatedComment,
+    });
+  }
+
+  return next(
+    new ForbiddenError('Not allowed! This comment does not belong to you')
+  );
+});
+
+exports.deleteComment = asyncHandler(async (req, res, next) => {
+  const { id: commentId } = req.params;
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    return next(
+      new NotFoundError(`No comment found with that ID →→ ${commentId}`)
+    );
+  }
+
+  if (comment.user._id === String(req.user.id) || req.user.role === 'admin') {
+    await comment.remove();
+
+    return res.status(StatusCodes.NO_CONTENT).json({
+      status: 'success',
+      comment: null,
+    });
+  }
+});
+
+exports.sendSportUserIds = (req, res, next) => {
+  if (!req.body.user) req.body.user = req.user.id;
+  if (!req.body.sport) req.body.sport = req.params.id;
+
+  next();
+};
+
+exports.getComments = factory.getAll(Comment);
+exports.getComment = factory.getOne(Comment);
+exports.createComment = factory.createOne(Comment);
